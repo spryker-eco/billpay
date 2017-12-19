0ba3b60c63abfa8805e567642c2686af58dd8635
Mykyta Borysevych <mykyta.borysevych@spryker.com>
2017-12-19 11:37:40 +0000

    ECO-251 Billpay ECO-480 Tests
    * test fixes

--- tests/SprykerEcoTest/Zed/Billpay/Business/BillpayConfiguratorBuilder.php
+++ tests/SprykerEcoTest/Zed/Billpay/Business/BillpayConfiguratorBuilder.php
@@ -21,10 +21,10 @@ class BillpayConfiguratorBuilder
         $config[BillpayConstants::GATEWAY_URL] = 'https://test-api.billpay.de/xml/offline';
         $config[BillpayConstants::BILLPAY_MERCHANT_ID] = 'merchant';
 //
-//        $config[BillpayConstants::BILLPAY_PUBLIC_API_KEY] = 'a39eb681636dec360000008635';
+        $config[BillpayConstants::BILLPAY_PUBLIC_API_KEY] = 'public-api';
 //
-//        $config[BillpayConstants::BILLPAY_PORTAL_ID] = '8635';//prescore
-//        $config[BillpayConstants::BILLPAY_SECURITY_KEY] = 'IzjZ8hUwPQt6';//prescore
+        $config[BillpayConstants::BILLPAY_PORTAL_ID] = 'portal-id';//prescore
+        $config[BillpayConstants::BILLPAY_SECURITY_KEY] = 'security-key';//prescore
 
 //$config[BillpayConstants::BILLPAY_PORTAL_ID] = '8634';//pre-authorize
 //$config[BillpayConstants::BILLPAY_SECURITY_KEY] = 'H0s7zIONwsL9';//pre-authorize
@@ -32,10 +32,10 @@ class BillpayConfiguratorBuilder
 //        $config[BillpayConstants::BILLPAY_PUBLIC_API_KEY] = 'a39eb681636dec360000008635';
 
         $config[BillpayConstants::BILLPAY_MAX_DELAY_IN_DAYS] = 20;
-//        $config[BillpayConstants::USE_MD5_HASH] = 1;
-//        $config[BillpayConstants::USE_PRESCORE] = 1;
-//        $config[BillpayConstants::IS_TEST_MODE] = 1;
-//        $config[BillpayConstants::CUSTOMER_GROUP] = BillpayConstants::CUSTOMER_GROUP_B2C;
+        $config[BillpayConstants::USE_MD5_HASH] = 1;
+        $config[BillpayConstants::USE_PRESCORE] = 1;
+        $config[BillpayConstants::IS_TEST_MODE] = 1;
+        $config[BillpayConstants::CUSTOMER_GROUP] = BillpayConstants::CUSTOMER_GROUP_B2C;
         return $config;
     }
 }
